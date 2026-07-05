79a93ae223402338b9b613af9c6d6e061b0cd3c5
samsational-code <104665665+samsational-code@users.noreply.github.com>
2026-07-05 14:52:40 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "do-things-v1";
+const CACHE = "do-things-v3";
 const FILES = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
